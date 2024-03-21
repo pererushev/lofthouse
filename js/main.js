dca094ecdad5d9b15a50099cb8e56ff4c45df879
Roman Pererushev <roman.pererushev@gmail.com>
2024-03-21 16:05:46 +0300

    almost done

--- js/main.js
+++ js/main.js
@@ -8,3 +8,47 @@ navBtn.onclick = function () {
   nav.classList.toggle("header__top-row--mobile");
   document.body.classList.toggle("no-scroll");
 };
+
+/* Phone Mask */
+mask("[data-tel-input]");
+
+// Удаление "+" если больше ничего не введено, чтобы показать placeholder
+const phoneInputs = document.querySelectorAll("[data-tel-input]");
+phoneInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.value == "+") input.value = "";
+  });
+  input.addEventListener("blur", () => {
+    if (input.value == "+") input.value = "";
+  });
+});
+
+/* Подключение API Yandex maps */
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [59.943543, 30.338928],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 14,
+  });
+
+  var myPlacemark = new ymaps.Placemark(
+    [59.943543, 30.338928],
+    {},
+    {
+      iconLayout: "default#image",
+      iconImageHref: "/img/map/location-pin.svg",
+      iconImageSize: [40, 40],
+      iconImageOffset: [-3, -42],
+    }
+  );
+}
