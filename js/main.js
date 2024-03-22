91fa8f0e1fb04bb049059cfff96c373a2995abb0
Roman Pererushev <roman.pererushev@gmail.com>
2024-03-22 11:53:17 +0300

    final v0.1

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ phoneInputs.forEach((input) => {
 ymaps.ready(init);
 function init() {
   // Создание карты.
-  var myMap = new ymaps.Map("map", {
+  var map = new ymaps.Map("map", {
     // Координаты центра карты.
     // Порядок по умолчанию: «широта, долгота».
     // Чтобы не определять координаты центра карты вручную,
@@ -43,12 +43,34 @@ function init() {
 
   var myPlacemark = new ymaps.Placemark(
     [59.943543, 30.338928],
-    {},
+    {
+      balloonContent: `
+        <div class="balloon">
+          <div class="balloon__address">Наб. реки Фонтанки 10-15</div>
+          <div class="balloon__contacts">
+            <a href="tel:+78121234567">+8 (812) 123-45-67</a>
+          </div>
+        </div>
+    `,
+    },
     {
       iconLayout: "default#image",
       iconImageHref: "/img/map/location-pin.svg",
       iconImageSize: [40, 40],
-      iconImageOffset: [-3, -42],
+      iconImageOffset: [-20, -40],
     }
   );
+
+  map.controls.remove("geolocationControl"); // удаляем геолокацию
+  map.controls.remove("searchControl"); // удаляем поиск
+  map.controls.remove("trafficControl"); // удаляем контроль трафика
+  map.controls.remove("typeSelector"); // удаляем тип
+
+  // map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  // map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove("rulerControl"); // удаляем контрол правил
+  map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты (опционально)
+
+  map.geoObjects.add(myPlacemark);
+  myPlacemark.balloon.open();
 }
